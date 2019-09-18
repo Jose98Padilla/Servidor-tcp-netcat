67f9c6d9b51c8459253df2d9f0f45eccb95b0918
Jose Manuel Padilla Caro <a310901@uach.mx>
2019-09-18 04:08:43 -0600

    Se crea servidor TCP y su interaccion con netcat

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+const client = new net.Socket();
+
+client.connect(3000, '127.0.0.1', ()=>{
+    console.log('Socket conectado correctamente.');
+});
+
+client.on('data', (data)=>{
+    //console.log('Info: ' + data); //Concatenacion
+    console.log(`Info: ${data}`); //Interpolacion
+
+    client.destroy();
+});
+
+client.on('close', ()=>{
+    console.log('Se ha cerrado la conexión.');
+});
+
+
+//hacer chat
