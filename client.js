d35c5237a782c4c66be6957161387f6c4d80c2e9
Jose Manuel Padilla Caro <a310901@uach.mx>
2019-10-02 15:26:19 -0600

    Se modifica el cliente para hacer una especie de chat muy simple

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const net = require('net');
-
+const readline = require('readline');
 const client = new net.Socket();
 
 client.connect(3000, '127.0.0.1', ()=>{
@@ -8,11 +8,22 @@ client.connect(3000, '127.0.0.1', ()=>{
 
 client.on('data', (data)=>{
     //console.log('Info: ' + data); //Concatenacion
-    console.log(`Info: ${data}`); //Interpolacion
-
-    client.destroy();
+    console.log(`Server: ${data}`); //Interpolacion
 });
 
 client.on('close', ()=>{
     console.log('Se ha cerrado la conexión.');
 });
+
+
+//hacer chat
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (line) => {
+    client.write(line);
+});
+
+rl.prompt();
